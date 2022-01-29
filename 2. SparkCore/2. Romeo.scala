517134c817a28b74558c1ace08b6cbf9c2acea62
Sergio Couto <ingscc00@gmail.com>
2022-01-29 11:37:53 +0100

    improve notebooks

--- 2. SparkCore/2. Romeo.scala
+++ 2. SparkCore/2. Romeo.scala
@@ -1,4 +1,25 @@
 // Databricks notebook source
+// MAGIC %md
+// MAGIC ![Romeo](https://cdn.tatlerasia.com/asiatatler/i/hk/2020/01/24120707-romeo-juliet-zeffirelli-tatler0120_cover_1000x735.jpg)
+// MAGIC 
+// MAGIC 
+// MAGIC 1. Sube el fichero romeo.txt en Databricks, está explicado en el tutorial de Databricks 
+// MAGIC  
+// MAGIC 2. Usa el método sc.textFile para cargar el fichero
+// MAGIC   - La ruta será "dbfs:/FileStore/..."
+// MAGIC 
+// MAGIC 3. Muestra el número de líneas usando el método count
+// MAGIC 
+// MAGIC 4. Ejecuta el método collect sobre el RDD. ¿Qué devuelve?
+// MAGIC 
+// MAGIC 5. Imprímelo por pantalla
+// MAGIC   - Usa el método foreach pasándole la función que quieres que aplique (println)
+// MAGIC 
+// MAGIC 6. Usa la función map para obtener la longitud de cada línea
+// MAGIC   - Ejecuta de nuevo collect sobre el nuevo RDD e imprímelo de nuevo
+
+// COMMAND ----------
+
 val romeoRDD = sc.textFile("dbfs:/FileStore/input/text/romeo.txt")
 
 // COMMAND ----------
@@ -19,3 +40,4 @@ val romeoRDD = sc.textFile("dbfs:/FileStore/input/text/romeo.txt")
 // COMMAND ----------
 
 //Calcular tamaño e imprimir
+
--- 2. SparkCore/3. WordCount.scala
+++ 2. SparkCore/3. WordCount.scala
@@ -1,4 +1,54 @@
 // Databricks notebook source
+// MAGIC %md
+// MAGIC 
+// MAGIC ![Napoleon](https://compote.slate.com/images/a50cc726-c052-4740-84d0-62ddd7c834f0.jpg?height=346&width=568)
+// MAGIC 
+// MAGIC # WordCount - Guerra y Paz
+// MAGIC 
+// MAGIC El objetivo es contar el número de veces que se usa cada palabra en el texto
+// MAGIC 
+// MAGIC * Sube el fichero WarAndPeace.txt en Databricks, está explicado en el tutorial de Databricks 
+// MAGIC  
+// MAGIC * Usa el método sc.textFile para cargar el fichero
+// MAGIC   - La ruta será "dbfs:/FileStore/..."
+// MAGIC 
+// MAGIC * Para obtener las palabras sigue el siguiente proceso
+// MAGIC   - Splitealo en palabras usando flatMap
+// MAGIC   - Elimina símbolos y pasa todo a mayúsculas. Usa map con .replaceAll("\\W+", "")
+// MAGIC   - Filtra contenidos vacíos mediante el método filter. Para saber si un String es vacío puedes usar isEmpty
+// MAGIC   - Deberías ntener ahora de un RDD de la siguiente forma:
+// MAGIC 
+// MAGIC ```
+// MAGIC +---------+
+// MAGIC |    value|
+// MAGIC +---------+
+// MAGIC |      the|
+// MAGIC |  project|
+// MAGIC |gutenberg|
+// MAGIC |    ebook|
+// MAGIC |       of|
+// MAGIC ```
+// MAGIC 
+// MAGIC 
+// MAGIC * En este punto ya tienes un RDD de palabras, necesitas contarlas. Para eso:
+// MAGIC   - Usa map para generar un RDD de pares, donde la clave sea la palabra y el valor 1. De esa forma luego podrás sumar las ocurrencias
+// MAGIC 
+// MAGIC ```
+// MAGIC +---------+----+
+// MAGIC |    _1   | _2 |
+// MAGIC +---------+----+
+// MAGIC |      the|  1 |
+// MAGIC |  project|  1 |
+// MAGIC |gutenberg|  1 |
+// MAGIC |    ebook|  1 |
+// MAGIC |       of|  1 |
+// MAGIC ```
+// MAGIC 
+// MAGIC * Utiliza el método reduceByKey visto en clase para agregar por clave. Por defecto Spark entiende que el primer elemento es la clave.
+// MAGIC * Utiliza el método sortBy para ordenarlo. Recuerda ordenarlo por el segundo elemento, el número de palabras
+
+// COMMAND ----------
+
 val inputPath = "dbfs:/FileStore/input/text/WarAndPeace.txt"
 val outputPath = "dbfs:/FileStore/output/WordCount"
 
@@ -16,4 +66,5 @@ val linesRDD = sc.textFile(inputPath)
 val wordsRDD = ???
 
 //display(wordsRDD.toDF)
+
 
