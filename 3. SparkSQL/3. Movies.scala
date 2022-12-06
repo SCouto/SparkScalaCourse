2c9345fd21da2d8bc46e11ac0065767f18a76843
Sergio Couto <ingscc00@gmail.com>
2022-12-06 16:38:35 +0100

    movies

--- 3. SparkSQL/3. Movies.scala
+++ 3. SparkSQL/3. Movies.scala
@@ -5,11 +5,13 @@ val usersPath = s"$basePath/users.dat"
 val moviesPath = s"$basePath/movies.dat"
 val ratingsPath = s"$basePath/ratings.dat"
 
-dbutils.fs.rm(outputPath, true)
-
-
 //Aux
-case class UserRating (userId: Int, gender: String, age: Int, occupation: String, zipcode: String, movie: Int, score: Int, tm: Long)
+//case class UserRating (userId: Int, gender: String, age: Int, occupation: String, zipcode: String, movie: Int, score: Int, tm: Long)
+
+
+case class Movie(id: Int, name: String, genre: String)
+case class User(userId: Int, gender: String, age: Int, occupation: String, zipcode: String)
+case class Rating(userId: Int, movie: Int, score: Int, tm: Long)
 
 
 import org.apache.spark.sql.functions._
@@ -19,21 +21,61 @@ import org.apache.spark.sql.types._
 
 //Load data
 
+val moviesDS = spark.read.option("delimiter", "::").option("inferSchema", "true").option("header", "true").csv(moviesPath).as[Movie]
+val usersDS = spark.read.option("delimiter", "::").option("inferSchema", "true").option("header", "true").csv(usersPath).as[User]
+val ratingsDS = spark.read.option("delimiter", "::").option("inferSchema", "true").option("header", "true").csv(ratingsPath).as[Rating]
 
+
+display(moviesDS)
 
 // COMMAND ----------
 
 //Películas por año
 val moviesWYear = ???
+
 val yearlyGrouped = ???
 
 display(yearlyGrouped)
 
 
 // COMMAND ----------
+//Writing output
+
+
+dbutils.fs.rm(outputPath, true)
+spark.sql("drop table if exists moviesByYear")
+
+moviesWYear.write.mode(SaveMode.Overwrite).partitionBy("year").csv(outputPath)
+
+moviesWYear.write.format("delta").partitionBy("year").saveAsTable("moviesByYear")
+
+
+
+// COMMAND ----------
+
+
+// MAGIC %sql
+// MAGIC --Seleccionando todo
+// MAGIC select * from default.moviesByYear
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC --Seleccionando filtro por año
+// MAGIC select * from default.moviesByYear
+// MAGIC where year = 1985
+
+// COMMAND ----------
+//Eliminando salida
+dbutils.fs.rm(outputPath, true)
+spark.sql("drop table if exists default.moviesByYear")
+
+// COMMAND ----------
 
 //Average rating by user
 val avgRatingsByUser = ???
+
+
 
 display(avgRatingsByUser)
 
